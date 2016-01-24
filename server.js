31965e4a00a7ea0defe649ee9e5d52d09d078a9c
= <renatoribroman@gmail.com>
2016-01-24 12:42:12 -0200

    server config

--- server.js
+++ server.js
@@ -3,22 +3,15 @@ var app = express();
 var tibia = require('./src/tibia');
 
 app.get('/login', function (req, res) {
-    tibia.login("arjenrobben", "88147358r", function(success, r, b){
-        res.send(b);
-    });
+    // testing propouses
 });
 
 app.get('/character/create', function (req, res) {
-    tibia.createCharacter("Hallsiny", "male", "Quilia", function(success, r, b){
-        res.send(b);
-    });
+    // testing propouses
 });
 
 app.get('/account', function (req, res) {
-    tibia.account(function(e, r, b){
-        //res.send(r.headers['set-cookie']);
-        res.send(b);
-    });
+    // testing propouses
 });
 
 app.listen(4545, function () {
